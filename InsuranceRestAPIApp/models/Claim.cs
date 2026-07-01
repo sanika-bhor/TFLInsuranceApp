7e3cb723003ebef439ae6d7e062825fcab96ccd0
sanika-bhor <bhorsanika0239@gmail.com>
2026-07-01 19:40:17 +0530

    insuranace application

--- InsuranceRestAPIApp/models/Claim.cs
+++
@@ -1,8 +0,0 @@
-namespace MaxNewYorkInsurance.Models;
-
-public class Claim
-{
-    public string? PolicyNumber { get; set; }
-    public decimal ClaimAmount { get; set; }
-    public string? Reason { get; set; }
-}
--- InsuranceRestAPIApp/models/Premium.cs
+++
@@ -1,8 +0,0 @@
-namespace MaxNewYorkInsurance.Models;
-
-public class Premium 
-{
-    public string? PolicyNumber { get; set; }
-    public decimal ClaimAmount { get; set; }
-    
-}
